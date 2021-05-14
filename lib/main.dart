c6114eababcb7522fa32e88b24cfc4e7d53ac0a3
tsaurav18 <tsaurav1711@gmail.com>
2021-05-14 20:49:07 +0900

    chapter4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,115 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "myapp",
-    home: HomePage(),
-  ));
+  runApp(
+    MaterialApp(
+      title: "myapp",
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.purple),
+    ),
+  );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _nameContoller = TextEditingController();
+  var myText = 'Change me';
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("myapp"),
-        ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(8),
-            alignment: Alignment.center,
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 5,
-                      offset: Offset(2.0, 5.0)),
-                ],
-                color: Colors.teal,
-                gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
-            child: Text(
-              "i'm a box",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("myapp"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  "assets/bg.jpeg",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameContoller,
+                    decoration: InputDecoration(
+                        hintText: "Enter Your name",
+                        labelText: "Name",
+                        border: OutlineInputBorder()),
+                  ),
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("saurav"),
+              accountEmail: Text("tsaurav1711@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80"),
+              ),
+            ),
+
+            // DrawerHeader(
+            //   child: Text(
+            //     "hi i am drawer",
+            //     style: TextStyle(color: Colors.white),
+            //   ),
+
+            //   decoration: BoxDecoration(color: Colors.purple),
+            // ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("tsaurav18@gmail.com"),
+              trailing: Icon(Icons.send),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameContoller.text;
+          setState(() {});
+        },
+        child: Icon(Icons.refresh),
+        backgroundColor: Colors.green,
+      ),
+    );
   }
 }
